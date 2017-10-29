1bf647865d76fd42dcf3a0e8f520be602027d7d0
SmileyJoe <cody.strugnell@gmail.com>
2017-10-29 23:09:05 +0200

    Initial commit, index, 404, requirements

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def custom_404(request):
+    return render(request, 'main/404.html')
