3a0719042caf0587d7e36d18899de27e3b6d9822
SmileyJoe <cody.strugnell@gmail.com>
2017-11-01 22:30:31 +0200

    Added model and database

--- secret/models.py
+++ secret/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime, timedelta
+
 
 # Create your models here.
+class Secret(models.Model):
+    hash = models.CharField(max_length=32)
+    secret = models.CharField(max_length=256)
+    expires = models.DateTimeField(default=datetime.now()+timedelta(days=3))
+
+    def __str__(self):
+        return self.secret
