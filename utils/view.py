f7209c1d85531a6300bcb57a92ee5cba97f994cf
SmileyJoe <cody.strugnell@gmail.com>
2017-11-19 11:43:36 +0200

    Cleaning up js analytics code

--- utils/view.py
+++ utils/view.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from smileyjoe_io import settings
 
+analytics = """
+        <script>
+            var GA_PAGE = '{ga_page}';
+            var GA_TRACKING_ID = '{ga_tracking_id}';
+        </script>
+        <script async src='https://www.google-analytics.com/analytics.js'></script>"""
+
 
 def display(request, **kwargs):
     if 'data' in kwargs:
@@ -9,8 +16,13 @@ def display(request, **kwargs):
         data = {}
 
     if 'ga_page' in kwargs:
-        data.update({'GA_PAGE': kwargs['ga_page']})
+        ga_page = kwargs['ga_page']
+    else:
+        ga_page = ""
 
     data.update({'GA_TRACKING_ID': settings.GA_TRACKING_ID})
+    data.update({'analytics': analytics.format(
+        ga_tracking_id=settings.GA_TRACKING_ID,
+        ga_page=ga_page)})
 
     return render(request, kwargs['page'], context=data)
