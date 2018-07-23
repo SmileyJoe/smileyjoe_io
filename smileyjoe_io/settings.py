29e3b55e33f17f799f59801158499809fcce0af4
SmileyJoe <cody.strugnell@gmail.com>
2018-07-23 21:06:01 +0200

    Updating allowed hosts

--- smileyjoe_io/settings.py
+++ smileyjoe_io/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SMILEYJOE_IO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('SMILEYJOE_IO_DEBUG').lower() == 'TRUE'.lower()
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'secret.localhost', '178.128.11.228']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'secret.localhost', '142.93.31.207']
 
 
 # Application definition
